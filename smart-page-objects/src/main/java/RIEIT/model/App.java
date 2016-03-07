aca5d36d45ab66567c1be4da960b3aeefd9403b2
Maninder Kaur <kaurmaninder53@gmail.com>
2016-03-07 19:53:01 +0530

    commiting login page and sign up page

---
+++ smart-page-objects/src/main/java/RIEIT/model/App.java
@@ -0,0 +1,17 @@
+package RIEIT.model;
+public class App {
+
+	public static void main(String[] args) {
+			
+		YahooHomePage yahoohomepage=  new YahooHomePage("http://www.yahoo.com");
+		
+		boolean s= yahoohomepage.isReady();
+		yahoohomepage.open();
+		yahoohomepage.reload();
+		yahoohomepage.error();
+		
+		System.out.println("Is page ready ?" + s );
+		
+	}
+
+}
---
+++ smart-page-objects/src/main/java/RIEIT/model/ILib.java
@@ -0,0 +1,13 @@
+package RIEIT.model;
+
+public interface ILib {
+
+	public String url();
+	
+	public boolean isReady();
+	
+	public void open();
+
+	public void reload();
+	
+}
